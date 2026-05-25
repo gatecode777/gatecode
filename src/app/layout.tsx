f230ed46f2bd0b8481288a5287c6238901b36bf3
gatecode777 <gatecodegit@gmail.com>
2026-05-25 18:20:29 +0530

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,10 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Gatecode Technologies',
   description: 'Empowering businesses with technology driven solutions.',
+  icons: {
+    icon: [{ url: '/images/favicon.png', type: 'image/png' }],
+    apple: '/images/favicon.png',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
